02c8f5c0a64a3ad508987e64bf7332c266f85da0
aniketjain8441 <aniketjain8441@gmail.com>
2022-09-19 21:45:08 +0530

    master

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -3,11 +3,16 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  void answerQuestion(){
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favorite color ? ',
-      'What\'s your favorite animal'
+      'What\'s your favorite color ?',
+      'What\'s your favorite animal ?'
     ];
     return MaterialApp(
         home: Scaffold(
@@ -17,8 +22,8 @@ class MyApp extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Text('The Question!'),
-          RaisedButton(child: Text('Answer 1'), onPressed: null),
-          RaisedButton(child: Text('Answer 2'), onPressed: null),
+          RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+          RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
           RaisedButton(child: Text('Answer 3 '), onPressed: null),
         ],
       ),
